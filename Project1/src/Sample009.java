f4c36724fd43f0322813e17f6dbf9c97ed6e9d5f
user <user@DESKTOP-1UI1DS0>
2025-09-02 20:36:57 +0900

    9/2 수업내용(배열까지)

--- Project1/src/Sample009.java
+++ Project1/src/Sample009.java
@@ -11,11 +11,16 @@ public class Sample009 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String[] movies = {
-				"One day", "F1"
-		};
+//		String[] movies = {
+//				"One day", "F1"
+//		};
 		
-		for (int i = 0; i < 1; i++) {
+		String[] movies = new String[2]; // 2편의 영화 배열 생성
+		movies[0] = "One day";
+		movies[1] = "F1" ;
+		
+		
+		for (int i = 0; i < 2; i++) {
 			System.out.println(movies[i]);
 			
 			
